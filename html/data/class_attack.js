2d24007affe68cdbf59ed5e18e63adc8afc448af
Ethan <ethan.merrien@etudiant.univ-rennes.fr>
2026-03-18 09:14:29 +0100

    mannaisse pespi

---
+++ html/data/class_attack.js
@@ -0,0 +1,23 @@
+class Attack {
+    static all_attacks = new Map();
+
+    constructor (id, nom, type, puissance, duree) {
+        this.id = id;
+        this.nom = nom;
+        this.type = type;
+        this.puissance = puissance;
+        this.duree = duree;
+    }
+    toString() { return `${this.nom} : ${this.id}), ${this.type}, ${this.puissance}, ${this.duree}ms` }
+
+    static fill_attacks() {
+        charged_moves.forEach(move => {
+            const attack = new Attack(move.move_id, move.name, move.type, move.power, move.duration);
+            this.all_attacks.set(move.move_id, attack);
+        });
+        fast_moves.forEach(move => {
+            const attack = new Attack(move.move_id, move.name, move.type, move.power, move.duration); 
+            this.all_attacks.set(move.move_id, attack);
+        });
+    }
+}
---
+++ html/data/class_pokemon.js
@@ -0,0 +1,33 @@
+class Pokemon {
+    static all_pokemons = new Map();
+
+    constructor(id, nom, stamina, attack, defense, types, fast_moves, charged_moves) {
+        this.id = id;
+        this.nom = nom;
+        this.stamina = stamina;
+        this.attack = attack;
+        this.defense = defense;
+        this.types = types;
+        this.fast_moves = fast_moves;
+        this.charged_moves = charged_moves;
+    }
+
+    toString() {
+        return `${this.nom} : #${this.id}, [${this.types.join(', ')}], [STA: ${this.stamina}, ATK: ${this.attack}, DEF: ${this.defense}], Rapides = [${this.fast_moves.map(m => m.nom).join(', ')}], Chargées = [${this.charged_moves.map(m => m.nom).join(', ')}]`;
+    }
+
+    getTypes() {
+        return this.types;
+    }
+
+    getAttacks() {
+        return [this.fast_moves, this.charged_moves];
+    }
+
+    static fill_pokemons() {
+        pokemons.forEach(pokemon => {
+            const pkmn = new Pokemon(pokemon.id, pokemon.name, pokemon.stamina, pokemon.attack, pokemon.defense, pokemon.types, pokemon.fast_moves, pokemon.charged_moves);
+            this.all_pokemons.set(pokemon.id, pkmn);
+        });
+    }
+}
